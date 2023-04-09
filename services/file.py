24ac02219f414fbc80caf0bea73c03829c9f27f9
andy <brickbazooka@gmail.com>
2023-04-09 16:50:48 -0500

    file debugging...

--- services/file.py
+++ services/file.py
@@ -47,8 +47,12 @@ def extract_text_from_filepath(filepath: str, mimetype: Optional[str] = None) ->
 def extract_text_from_file(file: BufferedReader, mimetype: str) -> str:
     if mimetype == "application/pdf":
         # Extract text from pdf using PyPDF2
+        print("Extracting text from pdf")
         reader = PdfReader(file)
+        print("Created Reader")
         extracted_text = " ".join([page.extract_text() for page in reader.pages])
+        print("Extracted text:")
+        print(extracted_text)
     elif mimetype == "text/plain" or mimetype == "text/markdown":
         # Read text from plain text file
         extracted_text = file.read().decode("utf-8")
